d8d77aca2f7bfe26d0483643b4acf710870a5d93
JuanManuelFM <jmfernad100@hotmail.com>
2022-11-12 23:23:10 -0300

    Rol y cabecera

--- Vista/Menu/Cabecera.php
+++ Vista/Menu/Cabecera.php
@@ -19,10 +19,10 @@
         <ul class="navbar-nav me-auto mb-2 mb-sm-0">
           
           <li class="nav-item">
-            <a class="nav-link active" href="buscarAuto.php" style="font-family: 'Chivo', sans-serif;">Inicio</a>
+            <a class="nav-link active" href="Inicio.php" style="font-family: 'Chivo', sans-serif;">Inicio</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="listaPersona.php" style="font-family: 'Chivo', sans-serif;">Tienda</a>
+            <a class="nav-link active" href="Tienda.php" style="font-family: 'Chivo', sans-serif;">Tienda</a>
           </li>
           <?php
           /*switch($rol){
