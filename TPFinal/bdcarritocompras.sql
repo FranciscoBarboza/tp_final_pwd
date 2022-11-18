4523708beb1003adf17ab693cbe438b2f38dcb49
FranciscoBarboza <francisco.barboza.tomas@gmail.com>
2022-11-18 17:06:06 -0300

    arreglo el sql

--- TPFinal/bdcarritocompras.sql
+++ TPFinal/bdcarritocompras.sql
@@ -289,17 +289,15 @@ CREATE TABLE IF NOT EXISTS `producto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 */
 
-insert into producto (`pronombre`,`prodetalle`, `proCantStock`, `proPrecio`, `urlImagen`)
-values ('AGUA BENDITA', '200ml frasco arrojadizo', 25, 700,  '');
 
-insert into producto (`pronombre`,`prodetalle`, `proCantStock`, `proPrecio`, `urlImagen`)
+insert into producto (`pronombre`,`prodetalle`, `proCantStock`, `proPrecio`, `urlItem`)
 values ('ESTACAS CAZAVAMPIROS', '35cm de pino tallado', 25, 500, 'https://i.pinimg.com/originals/3b/01/4a/3b014ad3e88e199cea4862a0efddca4b.jpg');
 
-insert into producto (`pronombre`,`prodetalle`, `proCantStock`, `proPrecio`, `urlImagen`)
+insert into producto (`pronombre`,`prodetalle`, `proCantStock`, `proPrecio`, `urlItem`)
 values ('BALAS DE PLATA', 'bendecidas', 100, 50, 'https://comunaslitoral.com.ar/06-2016/resize_1465306129.jpg');
 
-insert into producto (`pronombre`,`prodetalle`, `proCantStock`, `proPrecio`, `urlImagen`)
+insert into producto (`pronombre`,`prodetalle`, `proCantStock`, `proPrecio`, `urlItem`)
 values ('CRUCIFIJO', 'grabado y relieves en plata', 7, 800, 'https://www.antiguedadestecnicas.com/fotos/prin/AR-B-898/AR-B-898-1.jpg');
 
-insert into producto (`pronombre`,`prodetalle`, `proCantStock`, `proPrecio`, `urlImagen`)
-values ('EXORCISMO', 'viene un tipo a tu casa y te saca los fantasmas ', 'https://pbs.twimg.com/media/EY1UeCdWkAEsJKX.jpg')
+insert into producto (`pronombre`,`prodetalle`, `proCantStock`, `proPrecio`, `urlItem`)
+values ('EXORCISMO', 'viene un tipo a tu casa y te saca los fantasmas ', 20, 250,'https://pbs.twimg.com/media/EY1UeCdWkAEsJKX.jpg');
