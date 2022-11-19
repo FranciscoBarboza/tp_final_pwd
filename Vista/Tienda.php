8bb0316315bc058cce46fb1dcfc74cab23ec587f
FranciscoBarboza <francisco.barboza.tomas@gmail.com>
2022-11-18 22:40:01 -0300

    tienda semiestable completada

--- Vista/Tienda.php
+++ Vista/Tienda.php
@@ -5,10 +5,12 @@
 
 
     function crearTienda(){
-      $objProducto= new Producto();
-      $arrayProductos= $objProducto->listar();
+      $producto= new Producto();
+      $arrayProductos= $producto->listar();
 
-      
+      foreach ($arrayProductos as $objProducto) {
+        formato($objProducto);
+      }
 
 
 
@@ -17,8 +19,36 @@
     /**
      * imprime un 
      */
-    function formato($idProducto, ){
-        echo "";
+    function formato($objProducto){
+        
+
+        $idProducto= $objProducto->getIdProducto();
+        $proNombre= $objProducto->getProNombre();
+        $proDetalle= $objProducto->getProDetalle();
+        $proCantStock= $objProducto->getProCantStock();
+        $proPrecio= $objProducto->getProPrecio();
+        $urlItem= $objProducto->getUrlItem();
+
+        echo "
+        <div class=\"col-12 col-sm-12 col-md-4 col-lg-3 container py-2\" style=\"background-color: blue;\">
+        <div class=\"caja_producto id container col-9 col-sm-12 col-md-12 py-2\">
+            <div class=\"img_producto\">
+                <img src=\"{$urlItem}\" class=\"col-8 col-md-11 col-sm-9 \" height=\"\">
+            </div>
+            <div clas=\"titulo_producto text-start\"><h3 style=\"display: inline-block;\">{$proNombre}-{$proPrecio}</h3></div>
+            <form action=\"\">
+                <input type=\"number\" name=\"\" class=\"cantidad col-20\">
+                stock: {$proCantStock}
+                <button>COMPRAR</button>
+                <div class=\"d-none\">{$idProducto}</div>
+            </form>
+            
+            <hr>
+            <div class=\"desc_producto \">{$proDetalle}</div>
+
+        </div>
+    </div>
+        ";
     };
 
 
@@ -60,24 +90,26 @@
 -->
 
 
-<div class="container-fluid">  
+<div class="container-fluid" >  
 
     <div class="row" id="tienda_productos">
 
+    <?php crearTienda() ?>
+
+    <!--
     <div class="col-12 col-sm-12 col-md-4 col-lg-3 container py-2" style="background-color: blue;">
-        <div class="caja_producto id container col-9 col-sm-12 col-md-12 py-2">
+        <div class="caja_producto id container col-9 col-sm-11 col-md-12 py-2">
             <div class="img_producto">
                 <img src="https://i.pinimg.com/originals/3b/01/4a/3b014ad3e88e199cea4862a0efddca4b.jpg" class="col-8 col-md-11 col-sm-9 " height="">
             </div>
-            <div clas="titulo_producto text-start">titulo
-                
-            </div>
+            <div clas="titulo_producto text-start"><h3 style="display: inline-block;">titulo-$3500</h3></div>
             <form action="">
-                <input type="number" name="" class="cantidad col-2">
+                <input type="number" name="" class="cantidad col-3">
+                stock:
                 <button>COMPRAR</button>
             </form>
             
-            <hr>
+            <hr style="margin-top: 7px; margin-bottom: 0px;">
             <div class="desc_producto ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ullam nihil aliquid similique iste vel? Totam corrupti eligendi ipsum et.</div>
 
         </div>
@@ -86,7 +118,7 @@
     <div class="col-12 col-sm-12 col-md-4 col-lg-3" style="background-color: yellow;">
         <div class="caja_producto id container col-9 col-sm-12 col-md-12">
             <div class="img_producto">
-                <img src=<?php echo "aaaa" ?> class="col-8 col-md-11 col-sm-9" height="">
+                <img src= class="col-8 col-md-11 col-sm-9" height="">
             </div>
             <div clas="titulo_producto">titulo</div>
             <hr>
@@ -96,7 +128,7 @@
     </div>
     <div class="col-12 col-sm-12 col-md-4 col-lg-3" style="background-color: black;">aaaa</div>
     <div class="col-12 col-sm-12 col-md-4 col-lg-3" style="background-color: blue;">aaaa</div>
-
+-->
     </div>
 </div> 
 
