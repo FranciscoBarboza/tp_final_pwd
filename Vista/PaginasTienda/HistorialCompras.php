0a4e7950a788266e0adb8feea5e1269e3782172f
FranciscoBarboza <francisco.barboza.tomas@gmail.com>
2022-11-29 18:51:34 -0300

    ARREGLE LA CAGADA: LA VENGANZA

--- Vista/PaginasTienda/HistorialCompras.php
+++ Vista/PaginasTienda/HistorialCompras.php
@@ -1,4 +1,56 @@
 <?php
 include_once("../Menu/Cabecera.php");
 include_once("../../configuracion.php");
+
 ?>
+
+
+<html>
+<head>
+
+</head>
+
+<body>
+    
+
+
+<div class="container align-items-center " style="margin-top: 50px;">
+
+<table class="table table-hover table-bordered">
+  <thead class="">
+    <thead class="table-dark">
+      <th colspan="3" scope="col">idCompra</td>
+      <th colspan="1" scope="col">fecha iniciado</td>
+      <th colspan="1" scope="col">fecha terminada</th>
+    </thead>
+  </thead>
+  
+    
+  
+  <tbody>
+    <tr class="table-primary">
+      <th scope="col">id</th>
+      <th scope="col">nombre_producto</th>
+      <th scope="col">foto_prod</th>
+      <th scope="col">descripcion_prod</th>
+      <th scope="col">cant</th>
+      
+    </tr>
+
+    <div>
+  </div>
+  </tbody>
+</table>
+</div>
+
+
+
+
+
+
+
+
+</body>
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+</html>
