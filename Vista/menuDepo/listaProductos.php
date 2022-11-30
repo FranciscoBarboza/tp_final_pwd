e5e6edd2c46aa3cf789250a9998517e12a9ebe46
JuanManuelFM <jmfernad100@hotmail.com>
2022-11-30 15:39:02 -0300

    cambios listas

--- Vista/menuDepo/listaProductos.php
+++ Vista/menuDepo/listaProductos.php
@@ -24,7 +24,7 @@ include_once("../../configuracion.php");
                         <a class="btn text-decoration-none btn btn-outline-light" href="cargarProducto.php">AGREGAR PRODUCTO</a>
                     </div>
                     <?php
-                        if ($arrayProductos != null) {
+                        if ($arrayProductos != null){
                     ?>
                         <table class="table table-hover">
                             <thead class="text-center">
@@ -54,6 +54,8 @@ include_once("../../configuracion.php");
                                         echo '<td>'. $producto->getProCantStock().'</td>';
                                         echo '<td>'. $producto->getUrlItem().'</td>';
                                         echo '<td><a class="btn btn-dark" href="accionHabilitacionUsuario.php>Habilitar/Deshabilitar</a></td>'; 
+                                        echo '<td><a class = "remove" href="accion/accionEliminarProducto.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/></svg></a></td>';
+                                        echo '</tr>';
                                     }
                                 }else{
                                     echo '<p class="lead"> Actualmente no hay personas registradas </p>';
@@ -64,11 +66,17 @@ include_once("../../configuracion.php");
                             } ?>
                         </tbody>
                     </table>
+                    <?php
+                    }
+                    else{
+                        echo "<h3 class='text-danger'>No hay aún productos cargados en la página... Intentelo más tarde </h3>";
+                    }
+                    ?>
                 </div>
             </div>
     </div>
 </body>
 </html>
 <?php
-    include_once("Menu/Pie.php")
+    include_once("../menu/pie.php")
 ?>
