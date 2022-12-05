31c78f97c6b99276be2d44fde57b9e99d23012e5
JuanManuelFM <jmfernad100@hotmail.com>
2022-12-04 22:16:34 -0300

    cAMBIOS y mas cambios

--- Vista/menuDepo/listaProductos.php
+++ Vista/menuDepo/listaProductos.php
@@ -79,12 +79,12 @@ include_once("../../configuracion.php");
                         <table class="table table-hover">
                             <thead class="text-center">
                                 <tr>
+                                    <th>Imagen</th>
                                     <th>ID producto</th>
                                     <th>Nombre</th>
                                     <th>Detalle</th>
                                     <th>Precio</th>
                                     <th>En stock</th>
-                                    <th>URL imagen</th>
                                     <th></th>
                                     <th></th>
                                 </tr>
@@ -94,70 +94,72 @@ include_once("../../configuracion.php");
                                 if(isset($arrayProductos)){ //isset se fija si la variable tiene algo
                                     foreach ($arrayProductos as $producto){ 
                                         echo '<tr>';
-                                        echo '<td>'. $producto->getIdProducto().'</td>';
+                                        echo '<td><img src="'.$producto->getUrlItem().'" alt="" height="100" width="100"></td>';
+                                        echo '<td>' .$producto->getIdProducto().'</td>';
                                         echo '<td>'. $producto->getProNombre().'</td>';
                                         echo '<td>'. $producto->getProDetalle().'</td>';
                                         echo '<td>'. $producto->getProPrecio().'</td>';
                                         echo '<td>'. $producto->getProCantStock().'</td>';
-                                        echo '<td>'. $producto->getUrlItem().'</td>';
-                                        echo '<td><button type="button" class="btn btn-primary" data-bs-toggle="modal"data-bs-target="#exampleModal" data-bs-whatever="@mdo">Editar Producto</button>';
-                                        echo '<td><a class = "remove"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/></svg></a></td>';
+                                        echo '<td><button type="button" class="btn btn-success editarBoton" data-bs-toggle="modal"data-bs-target="#exampleModal" data-bs-whatever="@mdo">Editar Producto</button>';
+                                        echo '<td><button type="button" class="btn btn-warning remove">Deshabilitar</button></td>';
+                                        echo '<td><button type="button" class="btn btn-warning unRemove">Habilitar</button></td>';
                                         echo '</tr>';
                                     }
                                 }else{
-                                    echo '<p class="lead"> Actualmente no hay personas registradas </p>';
+                                    echo '<p class="lead"> Actualmente no hay productos registrados </p>';
                                 }
                             ?>
-                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">New message</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+            </div>
+                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role= "document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Modifique datos producto</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
                                 </div>
+
+                                <form action="accionActualizarProducto.php" class="needs-validation" method="POST">
+
                                 <div class="modal-body">
-                                    <form>
-                                    <div class="mb-3">
-                                        <label for="recipient-name" class="col-form-label">Recipient:</label>
-                                        <input type="text" class="form-control" id="recipient-name">
+                                    <div class="form-group">
+                                        <label>URL Imagen</label>
+                                        <input type="url" name="urlItem" id="urlItem" class="form-control" placeholder="Ingrese nuevo URL de la nueva imagen" required>
                                     </div>
-                                    <div class="mb-3">
-                                        <label for="message-text" class="col-form-label">Message:</label>
-                                        <textarea class="form-control" id="message-text"></textarea>
+                                    <input type="hidden" name="idProducto" id="idProducto">
+                                    <div class="form-group">
+                                        <label>Nombre Producto</label>
+                                        <input type="text" name="proNombre" id="proNombre" class="form-control" placeholder="Ingrese nuevo nombre de producto" required>
                                     </div>
-                                    </form>
+                                    <div class="form-group">
+                                        <label>Detalles</label>
+                                        <input type="text" name="proDetalle" id="proDetalle" class="form-control" placeholder="Ingrese nuevo detalle de producto" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Precio</label>
+                                        <input type="number" name="proPrecio" id="proPrecio" class="form-control" placeholder="Ingrese nuevo precio de producto" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Stock</label>
+                                        <input type="number" name="proCantStock" id="proCantStock" class="form-control" placeholder="Ingrese stock disponible de producto" required>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" name="insertData" class="btn btn-primary actualizar">Guardar</button>
+                                    </div>
                                 </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Send message</button>
-                                </div>
-                                </div>
+                                </form>
                             </div>
                         </tbody>
                     </table>
-                </div>
-            </div>
+</div>
 <!-- </body> -->
-<script>
-    (function () {
-  'use strict'
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
-</script>
+<script src="js/deshabilitarUsuario.js"></script>
+<script src="js/habilitarUsuario.js"></script>
+<script src="js/actualizarProducto.js"></script>
 <!-- </html> -->
 <?php
-    include_once("../menu/pie.php")
+    include_once("../menu/pie.php");
 ?>
