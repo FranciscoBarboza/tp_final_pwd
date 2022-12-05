fc9b22ae411a2654eb3e1918786957a62e1b76ee
FranciscoBarboza <francisco.barboza.tomas@gmail.com>
2022-12-05 10:43:31 -0300

    historial acabadop

--- Vista/menuCliente/HistorialCompras.php
+++ Vista/menuCliente/HistorialCompras.php
@@ -117,30 +117,20 @@ function crearModal($objCompra, $numero){
 
   $c_compra= new c_compraEstado();
   $arrayCompraEstados= $c_compra->buscar(["idCompra"=>$idCompra]);
-
-  print_r($arrayCompraEstados);
   $numero= intval($numero);
 
+  $string= "";
 
-  echo "
-  <div class=\"modal fade\" id=\"historia{$numero}\" tabindex=\"-1\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">
-  <div class=\"modal-dialog\">
-    <div class=\"modal-content\">
-      <div class=\"modal-header\">
-        <h5 class=\"modal-title\" id=\"exampleModalLabel\">Modal title</h5>
-        <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"modal\" aria-label=\"Close\"></button>
-      </div>
-      <div class=\"modal-body\">
-      
-
-<div class=\"container align-items-center \" >
+  $string=  "
 <table class=\"table table-hover table-bordered\">
   <thead>
     <th>estado</th>
     <th>fechaIni</th>
     <th>fechaFinal</th>
   </thead>
-  <tbody>";
+  <tbody>
+  
+  ";
 
 
 
@@ -148,9 +138,10 @@ function crearModal($objCompra, $numero){
 foreach ($arrayCompraEstados as $compraEstados) {
   //$compraEstados= new CompraEstado();
 
+
   //$compraEstadoTipo= new CompraEstadoTipo;
   //$compraEstados->getCeFechaFIN()
-  echo "
+  $string .= "
   
     <tr>
       <td>{$compraEstados->getObjCompraEstadoTipo()->getCetDescripcion()}</td>
@@ -163,20 +154,14 @@ foreach ($arrayCompraEstados as $compraEstados) {
   
 
 
-echo "
-
+$string.="
 </tbody>
 </table>
-</div>
-      </div>
-      <div class=\"modal-footer\">
-        <button type=\"button\" class=\"btn btn-secondary\" data-bs-dismiss=\"modal\">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+</div>";
 
-  ";
+  echo $string;
+
+
 }
 
 
@@ -198,9 +183,7 @@ echo "
 <?php
 crearhistorialCompras($idUsuarioAux);
 ?>
-
 <!-- 
-
 <div class="modal fade" id="historial1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -238,10 +221,8 @@ crearhistorialCompras($idUsuarioAux);
       </div>
     </div>
   </div>
-</div>
+</div> -->
 
-
- -->
 
 
 
